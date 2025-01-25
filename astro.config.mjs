fbdbd5e3b0d7b3b1f33ec37184d19a2c15b2c0cb
Jacob <104758482+JaFu-py@users.noreply.github.com>
2025-01-25 22:46:48 +0000

    Remove Astro DB

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,38 +6,44 @@ import react from "@astrojs/react";
 import sanity from "@sanity/astro";
 import vercel from "@astrojs/vercel";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
+	output: "static",
 
-    integrations: [svelte(), tailwind(), react(), sanity({
-        projectId: "mtg067gg",
-        dataset: "production",
-        projectId: "mtg067gg",
-        dataset: "production",
-        useCdn: true,
+	integrations: [
+		svelte(),
+		tailwind(),
+		react(),
+		sanity({
+			projectId: "mtg067gg",
+			dataset: "production",
+			projectId: "mtg067gg",
+			dataset: "production",
+			useCdn: true,
 
-        studioBasePath: "/sanity",
-		}), db()],
+			studioBasePath: "/sanity",
+		}),
+	],
 
-    server: {
-        port: 3210,
-        host: true,
-    },
+	server: {
+		port: 3210,
+		host: true,
+	},
 
-    vite: {
-        resolve: {
-            alias: {
-                $: "/src",
-            },
-        },
-    },
+	vite: {
+		resolve: {
+			alias: {
+				$: "/src",
+			},
+		},
+	},
+	experimental: {
+		svg: true,
+	},
 
-    adapter: vercel({
-        // isr: true,
-        // maxDuration: 7 * 24 * 60 * 60, // Weekly
-        // excludeFiles
-    }),
+	adapter: vercel({
+		// isr: true,
+		// maxDuration: 7 * 24 * 60 * 60, // Weekly
+		// excludeFiles
+	}),
 });
