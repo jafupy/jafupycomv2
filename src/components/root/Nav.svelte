859815f3579a973dde549c6c180b593ccaffae03
Jacob <104758482+JaFu-py@users.noreply.github.com>
2025-02-06 00:04:52 +0000

    Mobile Nav (yay finally)

---
+++ src/components/root/Nav.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+	import { assets, cn } from "$/lib";
+	import {
+		Command,
+		FolderKanban,
+		Hammer,
+		MenuIcon,
+		Newspaper,
+		User,
+		XIcon,
+	} from "lucide-svelte";
+
+	import Search from "./search-button.svelte";
+	import { fly } from "svelte/transition";
+
+	interface Props {
+		light?: boolean;
+	}
+
+	const { children, light, request } = $props();
+
+	let isNavOpen = $state(false);
+</script>
+
+<header class="fixed inset-x-4 top-4 z-50 p-0">
+	<nav
+		class={cn(
+			"not-prose group flex list-none items-center justify-center rounded-xl border p-1 text-sm backdrop-px gap-4",
+			light
+				? " border-grey-900/10 hover:border-grey-900/20"
+				: " border-grey-100/10 hover:border-grey-100/20"
+		)}
+	>
+		<a
+			href="/"
+			class="flex items-center justify-between gap-2 pr-4 mr-auto"
+		>
+			<img
+				src={assets.logo}
+				alt="Logo"
+				class="h-10 w-10 rounded-lg"
+			/>
+			<span class="text-sm">Jafu.py</span>
+		</a>
+		<ul class="items-center gap-2 hidden sm:flex">
+			<li class="group flex items-center">
+				<a href="/#bio">
+					<User
+						strokeWidth={1}
+						class="mr-2 inline h-4 w-4 text-old-rose"
+					/>
+					Bio</a
+				>
+			</li>
+			<li class="group-[li] flex items-center mx-4">
+				<a href="/blog?projects">
+					<FolderKanban
+						strokeWidth={1}
+						class="mr-2 inline h-4 w-4 text-old-rose"
+					/>
+					Projects</a
+				>
+			</li>
+			<li class="group flex items-center">
+				<a href="/blog"
+					><Newspaper
+						strokeWidth="1"
+						class="mr-2 inline h-4 w-4 text-old-rose"
+					/> Blog</a
+				>
+			</li>
+
+			<Search {request} />
+		</ul>
+		<button
+			onclick={() => (isNavOpen = !isNavOpen)}
+			class="flex items-center justify-center rounded-lg h-10 aspect-square m-0 p-2 border border-grey-100/20 sm:hidden relative"
+		>
+			{#if !isNavOpen}
+				<span
+					in:fly={{ duration: 100, y: -20 }}
+					out:fly={{ duration: 100, y: 20 }}
+					class="absolute inset-0 flex items-center justify-center"
+				>
+					<MenuIcon
+						strokeWidth="1"
+						class="h-4 w-4"
+					/>
+				</span>
+			{:else}
+				<span
+					class="absolute inset-0 flex items-center justify-center"
+					in:fly={{ duration: 100, y: 20 }}
+					out:fly={{ duration: 100, y: -20 }}
+				>
+					<XIcon
+						strokeWidth="1"
+						class="h-4 w-4"
+					/>
+				</span>
+			{/if}
+		</button>
+	</nav>
+</header>
+{#if isNavOpen}
+	<ul
+		class="items-center gap-4 text-base flex flex-col fixed inset-x-4 bottom-4 top-1/3 bg-black/20 px-12 py-5 rounded-xl border border-grey-100/10 backdrop-blur-md"
+	>
+		<li class="group flex items-center">
+			<a href="/#bio">
+				<User
+					strokeWidth={1}
+					class="mr-2 inline h-4 w-4 text-old-rose"
+				/>
+				Bio</a
+			>
+		</li>
+		<li class="group-[li] flex items-center mx-4">
+			<a href="/blog?projects">
+				<FolderKanban
+					strokeWidth={1}
+					class="mr-2 inline h-4 w-4 text-old-rose"
+				/>
+				Projects</a
+			>
+		</li>
+		<li class="group flex items-center">
+			<a href="/blog"
+				><Newspaper
+					strokeWidth="1"
+					class="mr-2 inline h-4 w-4 text-old-rose"
+				/> Blog</a
+			>
+		</li>
+
+		<Search
+			{request}
+			class="w-full flex-none"
+		/>
+	</ul>
+{/if}
--- src/components/root/search-button.svelte
+++ src/components/root/search-button.svelte
@@ -1,9 +1,19 @@
 <script>
+	import { cn } from "$/lib";
 	import { newToast } from "$/toasts.svelte";
 	import { Command } from "lucide-svelte";
 
+	const { request, class: className } = $props();
+
 	// import { isSearchOpen } from "$/lib";
 	import { onDestroy, onMount } from "svelte";
+
+	let isMac;
+	try {
+		isMac = navigator?.userAgentData?.platform === "macOS";
+	} catch (e) {
+		isMac = /Macintosh|Mac OS X/i.test(request);
+	}
 
 	const openSearch = () => {
 		// console.log("Click!");
@@ -19,28 +29,32 @@
 		// $isSearchOpen = !$isSearchOpen;
 	};
 	function handleKeydown(event) {
-		isMac = navigator.userAgentData.platform === "macOS";
 		if ((isMac ? event.metaKey : event.ctrlKey) && event.key === "k") {
 			event.preventDefault(); // Prevents default browser behaviour
 			openSearch();
 		}
 	}
 
-	onMount(() => {
+	$effect(() => {
 		window.addEventListener("keydown", handleKeydown);
-	});
-	onDestroy(() => {
-		window.removeEventListener("keydown", handleKeydown);
+
+		return () => {
+			window.removeEventListener("keydown", handleKeydown);
+		};
 	});
 </script>
 
 <button
-	on:click={openSearch}
-	class=" cursor-not-allowed items-center rounded-lg border border-grey-100/10 px-3 py-1 h-10 text-sm opacity-50 md:flex"
+	disabled
+	onclick={openSearch}
+	class={cn(
+		"flex-nowrap cursor-not-allowed items-center rounded-lg border border-grey-100/10 px-3 py-1 flex-row h-10 text-sm opacity-50 md:flex",
+		className
+	)}
 	>Search
 	<span class="ml-2 flex items-center gap-1 font-mono text-xs">
 		<kbd>
-			{#if navigator.userAgentData.platform === "macOS"}
+			{#if isMac}
 				<Command class="h-3 w-3 text-grey-500" />
 			{:else}
 				ctrl
