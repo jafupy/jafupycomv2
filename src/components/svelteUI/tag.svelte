1486ee51c8e60d9f44d40a5c365205951d0db8f0
Jacob <104758482+JaFu-py@users.noreply.github.com>
2025-02-15 10:39:22 +0000

    new svelte components

---
+++ src/components/svelteUI/tag.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from "$/lib.ts";
+	import { Tag as TagIcon } from "lucide-svelte";
+
+	let {
+		class: className = "",
+		icon: Icon = TagIcon,
+		children,
+	}: { class?: string; icon?: typeof TagIcon | false; children: Function } = $props();
+</script>
+
+<span
+	class={cn(
+		"border-grey-100/10 group inline-flex items-center rounded-md border px-2 py-1 text-xs font-semibold",
+		className
+	)}
+>
+	{#if Icon}
+		<Icon
+			stroke-width={1}
+			class="text-grey-500 mr-2 h-4 w-4 group-hover:-rotate-12"
+		/>
+	{/if}
+	{@render children()}
+</span>
---
+++ src/components/svelteUI/typewrite.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let { text, ...props }: { text: string } = $props();
+	let newText = $state(text);
+	function hacker() {
+		let interval: any = null;
+
+		let iteration = 0;
+
+		clearInterval(interval);
+
+		interval = setInterval(() => {
+			newText = newText
+				.split("")
+				.map((letter, index) => {
+					if (index < iteration) {
+						return text[index];
+					}
+
+					return letters[Math.floor(Math.random() * 26)];
+				})
+				.join("");
+
+			if (iteration >= text.length) {
+				clearInterval(interval);
+			}
+
+			iteration += 1 / 5;
+		}, 30);
+	}
+</script>
+
+<span
+	onmouseover={hacker}
+	{...props}>{newText}</span
+>
